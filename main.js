a83d47a5b9d35cf99c177bf8bd10f0f9ee260c01
ASteinheiser <asteinhe@asu.edu>
2016-07-23 15:53:07 -0700

    final v

--- main.js
+++ main.js
@@ -1,13 +1,59 @@
 var five = require('johnny-five')
-var net = require('net');
+var keypress = require('keypress')
+var net = require('net')
 
-var socket = net.connect({host: 'ESPD45C6B.local', port: 3030});
+var socket = net.connect({host: 'ESPD45C6B.local', port: 3030})
 socket.on('connect', function(){
-  socket.emit('open', null);
-});
+  socket.emit('open', null)
+})
 
-var feather = new five.Board({port: socket});
+var feather = new five.Board({port: socket})
 
 feather.on('ready', function() {
-  console.log('hello')
-});
+  var left = new five.Servo.Continuous(14)
+  var right = new five.Servo.Continuous(13)
+
+  var turnLeft = function() {
+    left.cw(1)
+    right.cw(1)
+  }
+  var turnRight = function() {
+    left.ccw(1)
+    right.ccw(1)
+  }
+  var forward = function() {
+    left.cw(1)
+    right.ccw(1)
+  }
+  var reverse = function() {
+    left.ccw(1)
+    right.cw(1)
+  }
+  var stop = function() {
+    left.cw(0)
+    right.cw(0)
+  }
+
+  keypress(process.stdin)
+
+  process.stdin.on('keypress', function (ch, key) {
+    if (key && key.ctrl && key.name == 'c') {
+      process.exit()
+    }
+    if (key && key.name == 'up') {
+      forward()
+    }
+    if (key && key.name == 'down') {
+      reverse()
+    }
+    if (key && key.name == 'left') {
+      turnLeft()
+    }
+    if (key && key.name == 'right') {
+      turnRight()
+    }
+    if (key && key.name == 'space') {
+      stop()
+    }
+  })
+})
