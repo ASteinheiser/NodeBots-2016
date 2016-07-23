5ba30796595fda8675c3ab9ead26a6c2214db875
ASteinheiser <asteinhe@asu.edu>
2016-07-23 14:17:27 -0700

    more progres

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-var j5 = require('johnny-five')
+var five = require('johnny-five')
+var net = require('net');
+
+var socket = net.connect({host: 'ESPD45C6B.local', port: 3030});
+socket.on('connect', function(){
+  socket.emit('open', null);
+});
+
+var feather = new five.Board({port: socket});
+
+feather.on('ready', function() {
+  console.log('hello')
+});
